c7a0630ece45065f7272918650bc50b2983f5ac4
Mahiuddin <mahiuddin.noyon@gmail.com>
2022-09-09 12:05:08 +0600

    added index method and create view quote

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class QuoteController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Http::get("https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json")->json();
+
+        // $ch = Http::get("https://api.kanye.rest");
+        // $ch = curl_init("https://api.kanye.rest");
+        // return curl_exec($ch);
+
+
+
+
+        return view('quote', ['data' => $data]);
+    }
 }
